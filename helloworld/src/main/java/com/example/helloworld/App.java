4c20b956137d7663b33177c98882156213ca167a
tabraz <tabraz@stylishop.com>
2024-12-13 10:44:43 +0000

    Test pre-push hook fix

--- helloworld/src/main/java/com/example/helloworld/App.java
+++ helloworld/src/main/java/com/example/helloworld/App.java
@@ -6,8 +6,7 @@ package com.example.helloworld;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
     }
 }
