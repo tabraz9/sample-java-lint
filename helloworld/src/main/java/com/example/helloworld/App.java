5b75feb18a11030b9010b63ef7dc4d1e4c2b993d
tabraz <tabraz@stylishop.com>
2024-12-13 11:05:37 +0000

    Test pre-push hook fix20

--- helloworld/src/main/java/com/example/helloworld/App.java
+++ helloworld/src/main/java/com/example/helloworld/App.java
@@ -3,22 +3,17 @@ package com.example.helloworld;
 /**
  * Contains classes related to the HelloWorld application.
  */
-package com.example.helloworld;  // Ensure that this package has its own package-info.java file.
-
-public final class App {
+public class App {
     
     /**
-     * Private constructor to prevent instantiation.
+     * The entry point for the HelloWorld application.
+     *
+     * @param args the command-line arguments
      */
     private App() {
         // private constructor to prevent instantiation
     }
 
-    /**
-     * The entry point for the HelloWorld application.
-     * 
-     * @param args the command-line arguments (not used)
-     */
     public static void main(final String[] args) {
         System.out.println("Hello World!");
     }
